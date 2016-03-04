1f640919a6ae0f8986ad3f8774c8e144be243581
Ritesh Shrivastav <ritesh_shrv@live.com>
2016-03-04 15:19:32 +0530

    Migrate ShippingCarriers

--- carrier.py
+++ carrier.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 from trytond.pool import PoolMeta
+from trytond import backend
+from trytond.transaction import Transaction
 
 
 __metaclass__ = PoolMeta
@@ -10,6 +12,22 @@ __all__ = [
 
 class SaleChannelCarrier:
     __name__ = 'sale.channel.carrier'
+
+    @classmethod
+    def __register__(cls, module_name):
+        TableHandler = backend.get('TableHandler')
+        cursor = Transaction().cursor
+
+        # Use magento_instance_carrier table to hold this model
+        if TableHandler.table_exist(cursor, 'magento_instance_carrier'):
+            TableHandler.table_rename(
+                cursor, 'magento_instance_carrier', cls._table
+            )
+
+            table = TableHandler(cursor, cls, module_name)
+            table.column_rename('title', 'name')
+
+        super(SaleChannelCarrier, cls).__register__(module_name)
 
     def get_magento_mapping(self):
         """
